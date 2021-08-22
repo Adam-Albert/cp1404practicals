e5dafb3c639d3734db5ef34a08af09efd25bb479
Adam <adam.albert@my.jcu.edu.au>
2021-08-23 00:08:42 +1000

    add random score into broken_score programs main function

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,6 +3,8 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+import random
+
 
 def main():
     score = float(input("Enter score: "))
@@ -10,6 +12,11 @@ def main():
         print("Invalid")
         score = float(input("Enter score: "))
     grade = get_grade(score)
+    print(grade)
+
+    score = random.randint(0, 100)
+    grade = get_grade(score)
+    print(score)
     print(grade)
 
 
